d463e7f5e903345a06bbba3a7ef68cbae237a628
baljinder-sandhu <baljinder.sandhu@rea-group.com>
2020-08-23 12:55:30 +1000

    Add countdown timer with hardcoded end date

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import React, { Component } from 'react'
-export default class Timer extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Time Remaining: 3:00</h1>
-      </div>
-    )
-  }
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    const difference = +new Date(`${year}-09-15`) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [year] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+  return (
+    <div style={{
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }
+  }}>
+      <h1>Melbourne Covid Lockdown Timer</h1><br></br>
+      {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+    </div>
+  );
 }
+
+export default App;
