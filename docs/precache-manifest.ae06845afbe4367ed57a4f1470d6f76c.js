9f9ea23559116b187ef0933146c2fd3fe8c9ba13
baljinder-sandhu <baljinder.sandhu@rea-group.com>
2020-08-24 17:37:08 +1000

    Add docs folder

---
+++ docs/precache-manifest.ae06845afbe4367ed57a4f1470d6f76c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3407c93f9dd53a742524581ed60a98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7430cda8f901a44ee29e",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a20ff06b729efa982e1f",
+    "url": "/static/css/main.e11df38b.chunk.css"
+  },
+  {
+    "revision": "7430cda8f901a44ee29e",
+    "url": "/static/js/2.5b7bc807.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5b7bc807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a20ff06b729efa982e1f",
+    "url": "/static/js/main.5738ef56.chunk.js"
+  },
+  {
+    "revision": "0253b44275f4e174a24e",
+    "url": "/static/js/runtime-main.f4687e6c.js"
+  }
+]);
