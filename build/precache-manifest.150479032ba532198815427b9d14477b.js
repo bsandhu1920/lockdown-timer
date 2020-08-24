fe0a706076801c0518d90f94306ad44e15475db4
baljinder-sandhu <baljinder.sandhu@rea-group.com>
2020-08-24 17:34:33 +1000

    Add build folder

---
+++ build/precache-manifest.150479032ba532198815427b9d14477b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd8a89179c16fc63ee022f84bf5abdf",
+    "url": "/lockdown-timer/index.html"
+  },
+  {
+    "revision": "7430cda8f901a44ee29e",
+    "url": "/lockdown-timer/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "85788efad27521aae324",
+    "url": "/lockdown-timer/static/css/main.e11df38b.chunk.css"
+  },
+  {
+    "revision": "7430cda8f901a44ee29e",
+    "url": "/lockdown-timer/static/js/2.5b7bc807.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lockdown-timer/static/js/2.5b7bc807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85788efad27521aae324",
+    "url": "/lockdown-timer/static/js/main.53c971fd.chunk.js"
+  },
+  {
+    "revision": "428fcea4992dee819989",
+    "url": "/lockdown-timer/static/js/runtime-main.495db6e3.js"
+  }
+]);
